6b2cdf931d338438d03b3ff386e2a4f7dec6e794
KimHyunLo <leeas016@gmail.com>
2023-01-11 17:23:33 +0900

    fetch api

--- lib/controller/lang_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-import 'package:get/get.dart';
-
-class LangController extends GetxController {
-  void changeLanguage(var lang, var country) {
-    var locale = Locale(lang, country);
-    Get.updateLocale(locale);
-  }
-}
---
+++ lib/product_module/controllers/product_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:getx_tutorial/api_module/api_service.dart';
+import 'package:getx_tutorial/product_module/models/product_model.dart';
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await ApiService.fetchProducts();
+      if (products != null) {
+        productList.assignAll(products);
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
